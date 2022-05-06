4902f17cfe512ec53cc036ee425cb9d1c7f35990
ariel monti <djaroldito@gmail.com>
2022-05-05 22:16:18 -0300

    todo terminado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -93,10 +93,7 @@ function deleteAbc(cadena){
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
-  nuevoArr = arr.sort(function(a,b){
-            return a.length>b.length
-         });
-  return nuevoArr;
+  return arr.sort((a,b)=> a.length - b.length);
 }
   
 
